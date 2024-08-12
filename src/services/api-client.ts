45d5dbbd30dc874c70cb1c852d8344b3c948eb06
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-08-12 14:19:53 +0100

    Implemented a reusable API client using react-query

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,13 +1,28 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-export default axios.create({
+const instance = axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
     key: '73c259d69909483dbd6adbe0b5329805'
   }
 });
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) =>
+    instance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+}
+
+export default APIClient;
